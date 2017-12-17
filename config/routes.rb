4fab6be480d4cd42c222381e8b6fb7c795e01a93
sheren <sheren.t@hotmail.com>
2017-12-17 18:22:00 +1100

    basics.. nothing more yet..

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/new'
+
+  root :to => 'pages#home'
+
+  get '/home' => 'pages#home'
+  get '/snake' => 'pages#snake'
+
 end
