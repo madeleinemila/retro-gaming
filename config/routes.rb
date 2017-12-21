5d9f2a9d1027c24e961ddf5a405f4b339013966a
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-21 19:30:06 +1100

    fixed undecl vars and 404 routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   get '/pong' => 'pages#pong'
   get '/space' => 'pages#space_shooter'
 
-  # post '/space/:score' => 'pages#space_shooter_update'
-  # post '/snake/:score' => 'pages#snake_update'
+  post '/space/:score' => 'pages#space_shooter_update'
+  post '/snake/:score' => 'pages#snake_update'
 
 
   # mount ActionCable.server => '/snake/cable'
