f1d6eff9b8b5a7822ed2f397923a038a5ed68bd2
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-21 11:52:19 +1100

    added second controller to server.js

--- arduino-controllers/server.js
+++ arduino-controllers/server.js
@@ -13,12 +13,24 @@ const io = require('socket.io')(http);
 
 const serialport = new SerialPort("/dev/cu.usbmodem1411");  // maddi's LHS USB port
 serialport.on('open', function(){
-  console.log('Serial Port Opened');
+  console.log('1411 Serial Port Opened');
   serialport.on('data', function(data){
       console.log(data[0]); // logging to terminal
       io.emit('pressed', { pressed: data[0] });
   });
 });
+
+const serialportRHS = new SerialPort("/dev/cu.usbmodem1421");  // maddi's RHS USB port
+serialportRHS.on('open', function(){
+  console.log('1421 Serial Port Opened');
+  serialportRHS.on('data', function(data){
+      console.log(data[0] + 10); // logging to terminal
+      io.emit('pressed1421', { pressed1421: data[0] + 10 });
+  });
+});
+
+
+
 
 // needed to serve back up socket.io
 http.listen(3001, function(){
