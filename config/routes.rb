2eae7c0b0e66d2bc1631e343332e0533eda9b2b3
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-21 22:07:06 +1100

    updated urls and favicon

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   root :to => 'pages#home'
 
   get '/home' => 'pages#home'
-  get '/snake' => 'pages#snake'
-  get '/pong' => 'pages#pong'
-  get '/space' => 'pages#space_shooter'
+  get '/snaak' => 'pages#snake'
+  get '/ping' => 'pages#pong'
+  get '/roids' => 'pages#space_shooter'
 
-  post '/space/:score' => 'pages#space_shooter_update'
-  post '/snake/:score' => 'pages#snake_update'
+  post '/roids/:score' => 'pages#space_shooter_update'
+  post '/snaak/:score' => 'pages#snake_update'
 
 
   # mount ActionCable.server => '/snake/cable'
