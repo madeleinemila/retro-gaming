c6ab305937520e6b29e6436e5be80960abdeae8b
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2018-01-08 20:07:34 +1100

    temp remove roids greater than validation

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,5 @@
 class Score < ApplicationRecord
   validates :snaak, :numericality => { :greater_than_or_equal_to => :snaak_was }, :on => :update
 
-  validates :roids, :numericality => { :greater_than_or_equal_to => :roids_was }, :on => :update
+  # validates :roids, :numericality => { :greater_than_or_equal_to => :roids_was }, :on => :update
 end
